62ed74f52911a5dc71388017673450f8af7fdb26
Silom <valentin@eiveo.net>
2014-09-29 16:09:09 +0200

    Added cookie check on start

--- views/auth/controller/userInformation.js
+++ views/auth/controller/userInformation.js
@@ -5,6 +5,20 @@ module.exports = ['$scope', 'USER_ROLES', 'AuthService', function ($scope, USER_
   $scope.userRoles = USER_ROLES
   $scope.isAuthorized = AuthService.isAuthorized
 
+    // Login if there are cookies
+  if ($cookieStore.get('username') && $cookieStore.get('key') && !AuthService.isAuthenticated()) {
+    var username = $cookieStore.get('username')
+    var key = $cookieStore.get('key')
+    var role = 'user'
+
+    Session.create($cookieStore.get('key'), $cookieStore.get('username'), 'user')
+
+    $scope.currentUser = {
+      user: username,
+      role: role
+    }
+  }
+
   $scope.setCurrentUser = function (user) {
     $scope.currentUser = user
   }
