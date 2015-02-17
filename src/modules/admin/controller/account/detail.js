efbec7966e79ea527e1c42874562289f36c694ff
Silom <valentin@eiveo.net>
2015-02-17 09:58:49 +0100

    clean up

--- src/modules/admin/controller/account/detail.js
+++ src/modules/admin/controller/account/detail.js
@@ -7,31 +7,33 @@ module.exports = function($scope, $state, $stateParams, AdminService, momentjs) 
   }
 
   AdminService.getAccount(accountId, function (res) {
-    $scope.identity = res
+    $scope.account = res
   }, function (err) {
     $scope.error = err.message
   })
 
-  $scope.identitySubmit = function (data) {
-    var toSend = {
-      name: {
-        first: data.first,
-        middle: data.middle || '',
-        last: data.last
+  $scope.identity = {
+    submit: function (input) {
+      var toSend = {
+        name: {
+          first: input.first,
+          middle: input.middle || '',
+          last: input.last
+        }
       }
+      AdminService.updateAccount(accountId, toSend, function (res) {
+        $scope.identity.success = true
+      }, function (err) {
+        $scope.identity.error = err.message
+      })
     }
-    AdminService.updateAccount(accountId, toSend, function (res) {
-      $scope.identity.success = true
-    }, function (err) {
-      $scope.identity.error = err.message
-    })
   }
 
   $scope.user = {
     unlink: function () {
       AdminService.accountUnlinkUser(accountId, function (res) {
         $scope.user.success = 'The link to the user was removed.'
-        $scope.identity.user = null
+        $scope.account.user = null
       }, function (err) {
         $scope.user.error = err.message
       })
@@ -39,7 +41,7 @@ module.exports = function($scope, $state, $stateParams, AdminService, momentjs) 
     link: function (name) {
       AdminService.accountLinkUser(accountId, {username: name}, function (res) {
         $scope.user.success = 'User was added to the account.'
-        $scope.identity = res
+        $scope.account = res
       }, function (err) {
         $scope.user.error = err.message
       })
@@ -50,7 +52,7 @@ module.exports = function($scope, $state, $stateParams, AdminService, momentjs) 
   $scope.note = {
     submit: function (input) {
       AdminService.accountAddNote(accountId, {data: input}, function (res) {
-        $scope.identity = res
+        $scope.account = res
         $scope.note.data = null
       }, function (err) {
         $scope.note.error = err.message
