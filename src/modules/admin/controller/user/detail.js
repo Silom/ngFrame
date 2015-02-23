c076aa5d66a54bcdcb34a7b08c0919bbcd915469
Silom <valentin@eiveo.net>
2015-02-23 11:05:02 +0100

    added link and unlink to the user detail page

--- src/modules/admin/controller/user/detail.js
+++ src/modules/admin/controller/user/detail.js
@@ -32,6 +32,47 @@ module.exports = function($scope, $state, $stateParams, AdminService) {
     }
   }
 
+  $scope.roles = {
+    unlinkAdmin: function () {
+      AdminService.adminUnlinkUser($scope.identity.roles.admin.id, function (res) {
+        $scope.roles.success = 'The link to this admin was removed.'
+        $scope.identity.roles.admin = null
+      }, function (err) {
+        $scope.user.error = err.message
+      })
+    },
+    unlinkAccount: function () {
+      AdminService.accountUnlinkUser($scope.identity.roles.account.id, function (res) {
+        $scope.roles.success = 'The link to this Account was removed.'
+        $scope.identity.roles.account = null
+      }, function (err) {
+        $scope.roles.error = err.message
+      })
+    },
+    linkAdmin: function (adminId) {
+      AdminService.adminLinkUser(adminId, {username: $scope.identity.username}, function (res) {
+        $scope.roles.success = 'Admin was added to this User.'
+        $scope.identity.roles.admin = {
+          id: res._id,
+          name: res.name.first + (res.name.middle ? (' ' + res.name.middle) : '') +' '+ res.name.last
+        }
+      }, function (err) {
+        $scope.roles.error = err.message
+      })
+    },
+    linkAccount: function (accountId) {
+      AdminService.accountLinkUser(accountId, {username: $scope.identity.username}, function (res) {
+        $scope.roles.success = 'Account was added to this User.'
+        $scope.identity.roles.account = {
+          id: res._id,
+          name: res.name.first + (res.name.middle ? (' ' + res.name.middle) : '') +' '+ res.name.last
+        }
+      }, function (err) {
+        $scope.roles.error = err.message
+      })
+    }
+  }
+
   $scope.deleteUser = function () {
     if (confirm('Are you sure?')) {
       AdminService.deleteUser(userId, function (res) {
